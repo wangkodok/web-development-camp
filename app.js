533b1c069c2c2fc43b38a48bc271382c481055c2
wangkodok <wangkodok@gmail.com>
2023-11-30 18:01:35 +0900

    show 라우트 및 파라미터 받아와서 상세 페이지로 이동

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ app.get("/campgrounds", async (req, res) => {
   res.render("campgrounds/index", { campgrounds });
 });
 
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
+});
+
 app.listen("3000", () => {
   console.log("3000 port 성공");
   console.log("http://localhost:3000/");
