ccc53cd3a3118ce0a2655f4035c402d034e613ad
wangkodok <wangkodok@gmail.com>
2023-12-01 17:04:14 +0900

    npm i ejs-mate 설치 및 코드

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
@@ -13,6 +14,7 @@ async function main() {
 const app = express();
 const path = require("path");
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
