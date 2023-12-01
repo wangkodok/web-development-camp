6453622b8bf25915a86bc790c01eb38561ac23b2
wangkodok <wangkodok@gmail.com>
2023-12-01 18:48:19 +0900

    이미지 추가 및 코드

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus qui velit in ipsum sunt eveniet facilis cupiditate vitae eligendi, nostrum assumenda atque ut dolores, quisquam odio deleniti libero sapiente dolore?",
+      price: price,
     });
     await camp.save();
   }
